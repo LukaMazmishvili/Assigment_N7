ae0ba73717433b28cea8b42ed4b23634e8607e25
Luka Mazmishvili <lukka.mazmishvili@gmail.com>
2023-11-26 22:20:04 +0400

    Implemeted bottom navigation view with 4 fragments

---
+++ app/src/main/java/com/example/luka_mazmishvli_7/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.luka_mazmishvli_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        val navController = findNavController(R.id.navHost)
+
+        bottomNavView.setupWithNavController(navController)
+    }
+}
